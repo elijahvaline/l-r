2d0e47123a5f52fa1cb28f3646d706baeeb882d3
Eli <elijahvaline@gmail.com>
2020-05-23 16:11:30 -0500

    first commit

---
+++ lur/lur/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lur
+//
+//  Created by Elijah Valine on 5/23/20.
+//  Copyright © 2020 Elijah Valine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
