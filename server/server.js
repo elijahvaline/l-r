4faead4fbca7f4bf1b6faf8ec5b6b7d716cbc142
Eli <elijahvaline@gmail.com>
2020-07-12 18:57:23 -0500

    Added server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Lur Server listening at http://%s:%s", host, port)
+})
+
+app.get('/', function (req, res) {
+   res.send("my string heres");
+})
+
